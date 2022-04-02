81902456a86fb6ebc7cf4ae21b1fde89d7c8eb92
D <1285434409@qq.com>
2022-04-02 11:27:44 +0800

    CountDownLatch实现线程执行时计数功能

---
+++ src/com/dk/juc/CountDownLatchDemo.java
@@ -0,0 +1,27 @@
+package com.dk.juc;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * CountDownLatch实现计数功能，当计数完成之后，主线程才会结束
+ */
+public class CountDownLatchDemo {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(6);
+        for (int i = 0; i < 6; i++) {
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + "\t离开教室");
+                countDownLatch.countDown();
+            }, String.valueOf(i)).start();
+        }
+        countDownLatch.await();
+        System.out.println(Thread.currentThread().getName() + "\t班长关门走人");
+    }
+
+    private static void closeDoor() {
+        for (int i = 0; i < 6; i++) {
+            new Thread(() -> System.out.println(Thread.currentThread().getName() + "\t离开教室"), String.valueOf(i)).start();
+        }
+        System.out.println(Thread.currentThread().getName() + "\t班长关门走人");
+    }
+}
